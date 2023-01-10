ca051a9dc127c00bf73aa407172395a9d7045429
lis0jk <lissjkk@gmail.com>
2023-01-10 13:36:05 +0300

    Edited sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    class Slider {
-        constructor(owlElement, owlOptions) {
-            this.owlElement = owlElement;
-            this.owlOptions = owlOptions;
-        }
-
-        addSlider(owlElement, owlOptions) {
-            $(owlElement).owlCarousel(owlOptions);
-        }
+  class Slider {
+    constructor(owlElement, owlOptions) {
+      this.owlElement = owlElement;
+      this.owlOptions = owlOptions;
     }
 
-    const mainPageSlider = new Slider();
+    addSlider(owlElement, owlOptions) {
+      $(owlElement).owlCarousel(owlOptions);
+    }
+  }
 
-     mainPageSlider.addSlider(".mainSlider", {
-        items: 1,
-        animateOut: 'fadeOut',
-        mouseDrag: false,
+  const mainPageSlider = new Slider();
+
+  mainPageSlider.addSlider(".mainSlider", {
+    items: 1,
+    animateOut: "fadeOut",
+    mouseDrag: false,
+  });
+
+  function openMobileSidebar() {
+    const burgerBtn = document.querySelector(".header__burger");
+    const closeMenuBtn = document.querySelector(".sidebar__close");
+    const mobMenu = document.querySelector(".sidebar");
+
+    burgerBtn.addEventListener("click", () => {
+      mobMenu.style.transform = "translateX(0%)";
     });
 
+    closeMenuBtn.addEventListener("click", () => {
+      mobMenu.style.transform = "translateX(100%)";
+    });
+
+    mobMenu.addEventListener("click", (event) => {
+      if (event.target) {
+        mobMenu.style.transform = "translateX(100%)";
+      }
+    });
+  }
+  openMobileSidebar();
 });
