6b2c1b092ed820b69f29807e2c27e298add4db14
Daniel <dan.bisyukov@gmail.com>
2023-01-20 21:43:56 +0300

    Added info to map's modal on main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,124 +1,150 @@
 document.addEventListener("DOMContentLoaded", function () {
-  class Slider {
-    constructor(owlElement, owlOptions) {
-      this.owlElement = owlElement;
-      this.owlOptions = owlOptions;
+    class Slider {
+        constructor(owlElement, owlOptions) {
+            this.owlElement = owlElement;
+            this.owlOptions = owlOptions;
+        }
+
+        addSlider(owlElement, owlOptions) {
+            $(owlElement).owlCarousel(owlOptions);
+        }
     }
 
-    addSlider(owlElement, owlOptions) {
-      $(owlElement).owlCarousel(owlOptions);
-    }
-  }
+    const mainPageBannerSlider = new Slider();
+    const mainPageNewsSlider = new Slider();
 
-  const mainPageBannerSlider = new Slider();
-  const mainPageNewsSlider = new Slider();
-
-  mainPageBannerSlider.addSlider(".bannerSlider", {
-    items: 1,
-    animateOut: "fadeOut",
-    mouseDrag: false,
-  });
-
-  mainPageNewsSlider.addSlider(".newsSlider", {
-    responsive: {
-      0: {
+    mainPageBannerSlider.addSlider(".bannerSlider", {
         items: 1,
-        margin: 20,
-      },
-      500: {
-        items: 2,
-        margin: 30,
-      },
-      1100: {
-        items: 3,
-        margin: 40,
-      },
-    },
-  });
-
-  function openMobileSidebar() {
-    const burgerBtn = document.querySelector(".header__burger");
-    const closeMenuBtn = document.querySelector(".sidebar__close");
-    const mobMenu = document.querySelector(".sidebar");
-
-    burgerBtn.addEventListener("click", () => {
-      mobMenu.style.transform = "translateX(0%)";
+        animateOut: "fadeOut",
+        mouseDrag: false,
     });
 
-    closeMenuBtn.addEventListener("click", () => {
-      mobMenu.style.transform = "translateX(-200%)";
-    });
-  }
-  openMobileSidebar();
-
-  function openHelpForm() {
-    const formBtn = document.querySelectorAll(".help__button");
-    const closeFormBtn = document.querySelectorAll(".modal__close");
-    const modalForm = document.querySelector(".modal");
-
-    formBtn.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        modalForm.style.display = "block";
-      });
-    });
-
-    closeFormBtn.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        modalForm.style.display = "none";
-      });
-    });
-  }
-  openHelpForm();
-
-  function dropDown() {
-    $(".sidebar__menu").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle();
+    mainPageNewsSlider.addSlider(".newsSlider", {
+        responsive: {
+            0: {
+                items: 1,
+                margin: 20,
+            },
+            500: {
+                items: 2,
+                margin: 30,
+            },
+            1100: {
+                items: 3,
+                margin: 40,
+            },
+        },
     });
 
-    $(".sidebar__menu-sub").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle();
-    });
+    function openMobileSidebar() {
+        const burgerBtn = document.querySelector(".header__burger");
+        const closeMenuBtn = document.querySelector(".sidebar__close");
+        const mobMenu = document.querySelector(".sidebar");
 
-    $(".header__menu").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle();
-    });
+        burgerBtn.addEventListener("click", () => {
+            mobMenu.style.transform = "translateX(0%)";
+        });
 
-    $(".header__menu-sub").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle();
-    });
-
-    $(".header__lung-ru").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle();
-    });
-
-    $(".sidebar__lung-ru").click(function (event) {
-      $(this).toggleClass("active").next().slideToggle();
-    });
-  }
-  dropDown();
-
-
-
-  const cities = document.querySelectorAll('.addresses__map-city');
-  const modalBG = document.querySelector('.addresses__modal-bg');
-  const modal = document.querySelector('.addresses__modal');
-  cities.forEach(city => {
-
-    city.addEventListener('click', (e)=> {
-        modal.querySelector('.addresses__modal-text').innerText = city.dataset.name;
-        modal.querySelector('.addresses__modal-img').setAttribute('src', city.dataset.img);
-        modal.style.top = (e.y + 70) + 'px';
-        modal.style.left = (e.x + 230) + 'px';
-        modalBG.classList.add('active');
-    });
-
-  });
-
-  document.addEventListener('click', (e) => {
-    if(e.target === modalBG){
-        modalBG.classList.remove('active');
+        closeMenuBtn.addEventListener("click", () => {
+            mobMenu.style.transform = "translateX(-200%)";
+        });
     }
-  });
+    openMobileSidebar();
 
+    function openHelpForm() {
+        const formBtn = document.querySelectorAll(".help__button");
+        const closeFormBtn = document.querySelectorAll(".modal__close");
+        const modalForm = document.querySelector(".modal");
+
+        formBtn.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                modalForm.style.display = "block";
+            });
+        });
+
+        closeFormBtn.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                modalForm.style.display = "none";
+            });
+        });
+    }
+    openHelpForm();
+
+    function dropDown() {
+        $(".sidebar__menu").click(function (event) {
+            $(this).toggleClass("active").next().slideToggle();
+        });
+
+        $(".sidebar__menu-sub").click(function (event) {
+            $(this).toggleClass("active").next().slideToggle();
+        });
+
+        $(".header__menu").click(function (event) {
+            $(this).toggleClass("active").next().slideToggle();
+        });
+
+        $(".header__menu-sub").click(function (event) {
+            $(this).toggleClass("active").next().slideToggle();
+        });
+
+        $(".header__lung-ru").click(function (event) {
+            $(this).toggleClass("active").next().slideToggle();
+        });
+
+        $(".sidebar__lung-ru").click(function (event) {
+            $(this).toggleClass("active").next().slideToggle();
+        });
+    }
+    dropDown();
+
+
+
+    const cities = document.querySelectorAll('.addresses__map-city');
+    const modalBG = document.querySelector('.cityInfo__bg');
+    const modal = document.querySelector('.cityInfo');
+
+    const cityImg = modal.querySelector('.cityInfo__address-img');
+    const cityAddress = modal.querySelector('.cityInfo__address-text');
+    const cityMap = modal.querySelector('.cityInfo__address-map');
+
+    class Cities {
+
+        constructor(img, name, address, map){
+            this.img = img;
+            this.name = name;
+            this.address = address;
+            this.map = map;
+        }
+
+        creatModal(img, name, address, map) {
+            modalBG.classList.add('active');
+
+            cityImg.setAttribute('src', img);
+            cityImg.setAttribute('alt', name);
+            cityAddress.innerText = address;
+            cityMap.setAttribute('href', map);
+        }
+        
+    }
+
+    cities.forEach(city => {
+        city.addEventListener('click', (e) => {
+            new Cities().creatModal(
+                city.dataset.img, 
+                city.dataset.name, 
+                city.dataset.address,
+                city.dataset.map
+            );
+
+            modal.style.top = (e.y + 30) + 'px';
+            modal.style.left = (e.x + 230) + 'px';
+        });
+    });
+
+    document.addEventListener('click', (e) => {
+        if (e.target === modalBG) {
+            modalBG.classList.remove('active');
+        }
+    });
 
 });
